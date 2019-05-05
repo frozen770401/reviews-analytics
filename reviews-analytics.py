7d7fc450d61a2ee93c96887b5f5b75590767edc0
YuCheng Duan <tuan770401@hotmail.com>
2019-05-05 17:52:27 +0800

    count

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -4,7 +4,19 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
 print(len(data))
-print(data[0])
+
+average = 0
+for count1 in data:
+	average += len(count1)
+
+print(average / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new))
+print(new[0])
